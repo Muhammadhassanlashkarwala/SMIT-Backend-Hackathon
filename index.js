b4d31bf5a86930fdd08c038d32213af3e9db24e7
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-01-25 21:01:10 +0500

    push

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./lib/dbConnect.js";
-import authRouter from "./routes/auth.route.js";
+import authRouter from "./routes/user.js";
+import userRouter from "./routes/user.js";
 
 let app = express();
 let port = 4000;
 
 app.use(cors());
 app.use(express.json())
-app.use("/api/auth", authRouter)
+app.use("/api/auth", userRouter)
 app.listen(port, () => {
     connectDB()
     console.log(`Server is running on port ${port}`);
